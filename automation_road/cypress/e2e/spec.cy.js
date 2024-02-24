8954bc1b54d5ed687e4668734a48632174c856b6
radrax <ianculescuionutradu@gmail.com>
2024-02-24 13:28:59 +0200

    Si asta e ceva ce am adaugat daaaaa

--- automation_road/cypress/e2e/spec.cy.js
+++ automation_road/cypress/e2e/spec.cy.js
@@ -1,5 +1,13 @@
 describe('template spec', () => {
   it('passes', () => {
+
     cy.visit('https://example.cypress.io')
-  })
-})
+    
+    cy.url()
+      .should('eq', 'https://example.cypress.io/')
+
+    cy.contains('h2', 'Commands')
+
+  });
+
+});
